6cda831dd691ed19e92b1c254911bb1beee35cf3
Md Mehedi Hasan <mehedihasan.cse18@gmail.com>
2025-11-14 17:01:15 +0600

    setup everything from zegocloud documentation

---
+++ lib/login_service.dart
@@ -0,0 +1,92 @@
+// Package imports:
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:zego_uikit_prebuilt_call/zego_uikit_prebuilt_call.dart';
+import 'package:zego_uikit_signaling_plugin/zego_uikit_signaling_plugin.dart';
+
+// Project imports:
+import 'common.dart';
+import 'constants.dart';
+
+/// local virtual login
+Future<void> login({
+  required String userID,
+  required String userName,
+}) async {
+  final prefs = await SharedPreferences.getInstance();
+  prefs.setString(cacheUserIDKey, userID);
+
+  currentUser.id = userID;
+  currentUser.name = 'user_$userID';
+}
+
+/// local virtual logout
+Future<void> logout() async {
+  final prefs = await SharedPreferences.getInstance();
+  prefs.remove(cacheUserIDKey);
+}
+
+/// on user login
+Future<void> onUserLogin() async {
+  /// 4/5. initialized ZegoUIKitPrebuiltCallInvitationService when account is logged in or re-logged in
+  await ZegoUIKitPrebuiltCallInvitationService().init(
+    appID: int.parse(dotenv.env['ZEGO_APP_ID'] ?? '0'),
+    appSign: dotenv.env['ZEGO_APP_SIGN'] ?? '',
+    userID: currentUser.id,
+    userName: currentUser.name,
+    plugins: [
+      ZegoUIKitSignalingPlugin(),
+    ],
+    config: ZegoCallInvitationConfig(
+      offline: ZegoCallInvitationOfflineConfig(
+        autoEnterAcceptedOfflineCall: false,
+      ),
+    ),
+    notificationConfig: ZegoCallInvitationNotificationConfig(
+      androidNotificationConfig: ZegoCallAndroidNotificationConfig(
+        showFullScreen: true,
+        fullScreenBackgroundAssetURL: 'assets/image/call.png',
+        callChannel: ZegoCallAndroidNotificationChannelConfig(
+          channelID: "ZegoUIKit",
+          channelName: "Call Notifications",
+          sound: "call",
+          icon: "call",
+        ),
+        missedCallChannel: ZegoCallAndroidNotificationChannelConfig(
+          channelID: "MissedCall",
+          channelName: "Missed Call",
+          sound: "missed_call",
+          icon: "missed_call",
+          vibrate: false,
+        ),
+      ),
+      iOSNotificationConfig: ZegoCallIOSNotificationConfig(
+        systemCallingIconName: 'CallKitIcon',
+      ),
+    ),
+    requireConfig: (ZegoCallInvitationData data) {
+      final config = (data.invitees.length > 1)
+          ? ZegoCallInvitationType.videoCall == data.type
+              ? ZegoUIKitPrebuiltCallConfig.groupVideoCall()
+              : ZegoUIKitPrebuiltCallConfig.groupVoiceCall()
+          : ZegoCallInvitationType.videoCall == data.type
+              ? ZegoUIKitPrebuiltCallConfig.oneOnOneVideoCall()
+              : ZegoUIKitPrebuiltCallConfig.oneOnOneVoiceCall();
+
+      config.avatarBuilder = customAvatarBuilder;
+
+      /// support minimizing, show minimizing button
+      config.topMenuBar.isVisible = true;
+      config.topMenuBar.buttons
+          .insert(0, ZegoCallMenuBarButtonName.minimizingButton);
+
+      return config;
+    },
+  );
+}
+
+/// on user logout
+void onUserLogout() {
+  /// 5/5. de-initialization ZegoUIKitPrebuiltCallInvitationService when account is logged out
+  ZegoUIKitPrebuiltCallInvitationService().uninit();
+}
---
+++ lib/my_app.dart
@@ -0,0 +1,16 @@
+// Flutter imports:
+
+import 'package:flutter/material.dart';
+import 'package:zegocloud_video_call/main.dart';
+
+class MyApp extends StatefulWidget {
+  final GlobalKey<NavigatorState> navigatorKey;
+
+  const MyApp({
+    required this.navigatorKey,
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() => MyAppState();
+}
